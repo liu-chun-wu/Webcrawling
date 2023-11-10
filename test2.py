e5aa5ef54c832a92840eefa69ec129027e8ca146
Jeffery Liu <a4264428@gmail.com>
2023-11-10 10:39:39 +0800

    hw4 update

--- test2.py
+++ test2.py
@@ -1,47 +1,20 @@
-from flask import Flask, request, abort
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+from selenium.webdriver.common.by import By
+# 設定瀏覽器選項
+options = Options()
+# 建立 driver
+s = Service()
+chrome = webdriver.Chrome(service=s, options=options)
+url = "https://www.google.com/"
+chrome.get(url)
+SearchBar = chrome.find_element(By.ID, "APjFqb")
+SearchBar.send_keys("戰車世界\ue007")
+# 存取 Website
 
-from linebot.v3 import WebhookHandler
-from linebot.v3.exceptions import InvalidSignatureError
-from linebot.models import FlexSendMessage
-from linebot.models.flex_message import BubbleContainer
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage,
-    FlexMessage,
-    FlexContainer
-)
-
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
-import requests
-from bs4 import BeautifulSoup
-import xml.etree.cElementTree as ET
-import json
-content = requests.get(
-    'https://invoice.etax.nat.gov.tw/invoice.xml')
-tree = ET.fromstring(content.text)
-items = list(tree.iter(tag='item'))
-title = items[0][0].text
-ptext = items[0][3].text
-ptext = ptext.replace('<p>', '').replace('</p>', ':')
-ptext = ptext.replace('、', ':').replace('：', ':')
-ptext = ptext.split(':')
-special_1 = ptext[0] + " : " + ptext[1]
-special_2_name = ptext[2]
-
-s3 = ptext[4] + ptext[5]
-s4 = ptext[4] + ptext[6]
-s5 = ptext[4] + ptext[7]
-templist = []
-templist.append(ptext[0] + " : " + ptext[1])
-templist.append(ptext[2])
-templist.append(ptext[3])
-templist.append(ptext[4])
-templist.append(ptext[5])
-templist.append(ptext[4])
-templist.append(ptext[6])
-templist.append(ptext[4])
-templist.append(ptext[7])
-print(templist)
+# 等待 5 秒鐘以載入頁面
+time.sleep(120)
+# 關閉瀏覽器視窗
+chrome.close()
